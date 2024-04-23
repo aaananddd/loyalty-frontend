909c7d85832802c0186451af02d5208fb738a695
dracu-lah <nevilnicks4321@gmail.com>
2024-04-23 16:29:23 +0530

    completed styling u

---
+++ src/pages/private/user-management/SearchBar/index.jsx
@@ -0,0 +1,52 @@
+import { Search } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { handleKeyDown } from "@/utils/functions/ctrlKSearch";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const SearchBar = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    document.addEventListener("keydown", (event) =>
+      handleKeyDown(event, inputRef),
+    );
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+  const handleChange = (event) => {
+    setSearchValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Search for:", searchValue);
+  };
+
+  return (
+    <form
+      className="flex items-center  justify-center   relative lg:mr-4 w-96"
+      onSubmit={handleSubmit}
+    >
+      <Input
+        type="text"
+        className="pl-2 lg:w-96 pr-20"
+        placeholder="Search Users...   (Ctrl+K)"
+        ref={inputRef}
+        value={searchValue}
+        onChange={handleChange}
+      />
+      <Button
+        type="submit"
+        onClick={() => inputRef.current.blur()}
+        className="absolute right-0 top-0"
+      >
+        <Search />
+      </Button>
+    </form>
+  );
+};
+
+export default SearchBar;
---
+++ src/pages/private/user-management/UsersList/index.jsx
@@ -0,0 +1,30 @@
+import { Card } from "@/components/ui/card";
+import { users } from "@/utils/constants";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+const UsersList = () => {
+  function UserCard({ name, email, phoneNumber }) {
+    return (
+      <Card className="flex justify-between items-center p-4">
+        <div className="flex justify-center items-center gap-x-4">
+          <Avatar className="size-16">
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+          <div>
+            <h1>{name}</h1>
+            <p className="text-sm text-black/50">{email}</p>
+            <p className="text-sm text-black/50">Phone: {phoneNumber}</p>
+          </div>
+        </div>
+        <div className="space-x-4">
+          <Button className="bg-yellow-500 ">Edit</Button>
+          <Button className="bg-red-500">Remove</Button>
+        </div>
+      </Card>
+    );
+  }
+  return users.map((user) => <UserCard key={user.userId} {...user} />);
+};
+
+export default UsersList;
