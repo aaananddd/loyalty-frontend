373c7bbb6d84568a77d581059c72208bb6d4bf2b
ANAND <anandchandran3306@gmail.com>
2024-04-23 17:13:31 +0530

    added  dashboard heading

--- src/pages/private/dashboard/index.jsx
+++ src/pages/private/dashboard/index.jsx
@@ -4,6 +4,7 @@ import UsersActivity from "./UsersActivity";
 const DashBoardPage = () => {
   return (
     <div className="p-4 space-y-4">
+      <h1 className="text-3xl font-semibold">Dashboard</h1>
       <UsersActivity />
       <div className="flex gap-x-4">
         <Overview /> 
