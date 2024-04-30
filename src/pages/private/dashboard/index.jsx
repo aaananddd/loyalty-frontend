9917c7a31de2175fa851561523fa2c62f34ab7c3
ANAND <anandchandran3306@gmail.com>
2024-04-30 14:43:56 +0530

    fixed dashboard

--- src/pages/private/dashboard/index.jsx
+++ src/pages/private/dashboard/index.jsx
@@ -6,8 +6,8 @@ const DashBoardPage = () => {
     <div className="p-4 space-y-4 w-full">
       <h1 className="text-3xl font-semibold">Dashboard</h1>
       <UsersActivity />
-      <div className="flex gap-x-4">
-        <Overview /> 
+      <div className="flex flex-col lg:flex-row gap-y-4 gap-x-4">
+        <Overview />
         <RecentSales />
       </div>
     </div>
